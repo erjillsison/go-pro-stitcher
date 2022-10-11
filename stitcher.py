cda4ac003c868d7d1c131830c033db19e7ffe6b1
Erjill Sison <erjillsison@gmail.com>
2022-10-11 20:03:16 +0800

    format

--- stitcher.py
+++ stitcher.py
@@ -2,33 +2,36 @@ import os
 
 files = os.listdir(".")
 
+
 class File:
-  def __init__(self, chapter, filename):
-    self.chapter = chapter
-    self.filename = filename
+    def __init__(self, chapter, filename):
+        self.chapter = chapter
+        self.filename = filename
+
 
 fs = {}
 
 for j in files:
     prefix = j[0:2]
-    
+
     if prefix != "GH":
         continue
 
-    print(j[0:2], end =" ")
+    print(j[0:2], end=" ")
 
-
-    print("chapter: ", j[3:4], end =" ")
+    print("chapter: ", j[3:4], end=" ")
     video_num = j[5:8]
     print("video number, ", video_num)
-        
+
     if video_num not in fs:
         fs[video_num] = []
 
-    fs[video_num].append(File(j[3:4],j))
+    fs[video_num].append(File(j[3:4], j))
 
-def key(f:File):
+
+def key(f: File):
     return f.chapter
+
 
 output_count = 0
 fmpeg_intermediate = "ffmpeg -i %s -c copy %s.ts"
@@ -40,22 +43,22 @@ try:
 
         if len(fs[j]) <= 1:
             continue
-        
+
         fs[j].sort(key=key)
-        
+
         intermediate_count = 0
         for i in fs[j]:
             print(fmpeg_intermediate % (i.filename,  intermediate_count))
             os.system(fmpeg_intermediate % (i.filename,  intermediate_count))
             intermediate_count = intermediate_count + 1
-        
+
         i_files = []
         for i in range(intermediate_count):
             i_files.append("%s.ts" % i)
-        
+
         print(ffmpeg_concat % ("|".join(i_files), j))
         os.system(ffmpeg_concat % ("|".join(i_files), j))
-        
+
         print(i_files)
 
         for i in range(intermediate_count):
